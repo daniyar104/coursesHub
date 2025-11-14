8e8536089622274682a023e23d15704d97093d0e
Daniyar <ukubidoma@gmail.com>
2025-11-14 10:28:27 +0500

    (feature): Selected courese page

--- src/pages/main/HomePage.tsx
+++ src/pages/main/HomePage.tsx
@@ -1,13 +1,8 @@
-import Header from "../../components/Header/HomeHeader.tsx";
-import CoursesList from "./coursesList/CoursesList.tsx";
-import PresentPage from "./present/PresentPage.tsx";
 
 function HomePage() {
     return (
         <>
-            {/*<Header />*/}
-            {/*<PresentPage />*/}
-            {/*<CoursesList/>*/}
+
         </>
     );
 }
